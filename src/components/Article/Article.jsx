572c5a5a33ace3a05274549024d9f5027f15b578
Yevhen <zhekafill@gmail.com>
2024-11-18 14:52:03 +0200

    solution2

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -7,19 +7,18 @@ function Article() {
       <h1 className="article__title">Headline</h1>
       <p className="article__paragraph">
         In elementum lorem eget est euismod ornare. Phasellus sit amet
-        pellentesque mauris. Aliquam quis malesuada ex. Nullam eu aliquam nibh.
-        Mauris molestie, urna accumsan ornare semper, augue nibh posuere lorem,
-        vitae feugiat sem magna eget massa. Vivamus quis tincidunt dolor. Fusce
+        pellentesque mauris. Aliquam quis malesuada ex. Nullam eu aliquam
+        molestie, urna accumsan ornare semper, augue nibh posuere lorem, lorem,
+        lorem, vitae feugiat sem magna eget massa. Vivamus quis tincidunt
         efficitur, orci non vestibulum consequat, lectus turpis bibendum odio,
-        in efficitur leo felis sed justo. Fusce commodo iaculis orci, quis
-        imperdiet urna. Sed mollis facilisis lacus non condimentum. Nunc
-        efficitur massa non neque elementum semper. Vestibulum lorem arcu,
-        tincidunt in quam et, feugiat venenatis augue. Donec sed tincidunt
-        tellus, a facilisis magna. Proin sit amet viverra nibh, bibendum gravida
-        felis. Vivamus ut nunc id mauris posuere pellentesque. Praesent
-        tincidunt id odio id feugiat.
+        odio, in efficitur leo felis sed justo. Fusce commodo iaculis orci, quis
+        imperdiet urna. Sed mollis facilisis lacus non condimentum. efficitur
+        massa non neque elementum semper. Vestibulum lorem tincidunt in quam et,
+        feugiat venenatis augue. Donec sed tincidunt tellus, a facilisis magna.
+        Proin sit amet viverra nibh, bibendum gravida felis. felis. Vivamus ut
+        nunc id mauris posuere pellentesque. Praesent tincidunt id odio id
+        feugiat.
       </p>
-
       <p className="article__paragraph">
         In ac nisi lacus. Fusce est dolor, tincidunt ut bibendum vitae,
         fermentum ac quam. Aliquam pretium tristique nibh quis iaculis. In et
@@ -28,9 +27,8 @@ function Article() {
         molestie. Morbi lacinia, sapien eu dictum dignissim, tellus tortor
         congue magna, sit amet bibendum libero nisi id massa.
       </p>
-
       <p className="article__paragraph">
-        Donec arcu elit, euismod vel lobortis eu, fringilla sit amet dolor. Cras
+        Donec arcu elit, euismod vel lobortis eu, fringilla sit amet dolor.
         congue, massa nec sagittis mollis, dui felis ultrices magna, tincidunt
         finibus lorem quam in sem. Morbi odio turpis, pulvinar sit amet
         vulputate quis, ultricies eu libero. Donec ac maximus neque, nec maximus
@@ -40,17 +38,15 @@ function Article() {
         consectetur. Fusce placerat ac ipsum ac eleifend. Aenean quis faucibus
         ex.
       </p>
-
       <p className="article__paragraph">
         Cras egestas tempor nibh, a fermentum lorem sollicitudin non. Nulla
         facilisi. In at elit id leo tristique condimentum. Donec at est nulla.
         Mauris egestas magna ut laoreet pretium. Sed ultrices suscipit
         vestibulum. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
         Fusce id sapien eros. Vivamus viverra ultricies gravida. Nam urna nibh,
-        blandit a vulputate at, vehicula non nulla. Aenean ut nulla leo.
+        nibh, blandit a vulputate at, vehicula non nulla. Aenean ut nulla leo.
         Praesent in ullamcorper est.
       </p>
-
       <p className="article__paragraph">
         Pellentesque habitant morbi tristique senectus et netus et malesuada
         fames ac turpis egestas. Phasellus bibendum nec arcu eu lobortis. Nam
@@ -60,9 +56,9 @@ function Article() {
         dictum. Aliquam erat ligula, fringilla nec suscipit sed, porta vitae
         turpis. Vestibulum rhoncus placerat nulla vitae suscipit. Curabitur
         consectetur ex ut odio tristique vehicula. Ut ligula tortor, tincidunt
-        quis sodales vitae, ornare a turpis. Proin sit amet finibus enim. Fusce
-        tempus a neque vitae tempor. Aenean rutrum, libero iaculis interdum
-        vulputate, dui eros vehicula nisi, at interdum enim lacus eu diam.
+        quis sodales vitae, ornare a turpis. Proin sit amet finibus enim.
+        tempor. Aenean rutrum, libero iaculis interdum vulputate, dui eros
+        vehicula nisi, at interdum enim lacus eu diam.
       </p>
     </article>
   );
